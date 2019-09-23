a7587e12f71c6793af498fb8d65ff3b1ebd4391d
alexanderson <alex.anderson@conversocial.com>
2019-09-23 16:25:59 +0100

    refactor: noticket - Add mongo secondaries to compose environment

--- sample_test_settings.py
+++ sample_test_settings.py
@@ -1,13 +1,13 @@
 CONN1 = {
-    'uri': 'mongodb://controller:27017/?replicaSet=cluster-1',
+    'uri': 'mongodb://replica_1a:27017,replica_1b:27017/?replicaSet=rs-1',
     'db_name': 'test_sharding'
 }
 CONN2 = {
-    'uri': 'mongodb://replica:27017/?replicaSet=cluster-2',
+    'uri': 'mongodb://replica_2a:27017,replica_2b:27017/?replicaSet=rs-2',
     'db_name': 'test_sharding'
 }
 CONTROLLER = {
-    'uri': 'mongodb://controller:27017/?replicaSet=cluster-1',
+    'uri': 'mongodb://controller:27017/?replicaSet=rs-0',
     'db_name': 'test_metadata'
 }
 
